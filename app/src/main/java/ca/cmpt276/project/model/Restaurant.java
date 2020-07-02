cf69470f857800fd453302b359cf7e1bee7a67f7
dhpham <dayvidpham@gmail.com>
2020-07-01 17:15:03 -0700

    Refactors the RestaurantManager class as a mapping between Tracking Numbers -> Restaurants instead of a simple list of Restaurants; can still get an immutable list of Restaurants from RestaurantManager with the restaurants() method; Restaurant is now (mostly) immutable class; Updates the project to allow Java 8 language features

--- app/src/main/java/ca/cmpt276/project/model/Restaurant.java
+++ app/src/main/java/ca/cmpt276/project/model/Restaurant.java
@@ -1,73 +1,50 @@
 package ca.cmpt276.project.model;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Restaurant {
-    private String rest_name;
-    private String rest_addr;
-    private String GPS_coord;
-    private String rest_tracking_num;
-    private int num_reports;
-    private List<Inspection> rest_inspectionList;
+    public  final String trackingNumber;     // unique ID
+    public  final String name;
+    public  final String address;
+    public  final String city;
+    // GPS coordinates
+    public  final double latitude;
+    public  final double longitude;
+
+    private final List<Inspection> inspections;
 
 
+    //////////////////////////////////////////////////////////////////////
+    // Constructor
 
-    //decide to add image in the UI, not in the class
-    public Restaurant(String rest_name, String rest_addr, String GPS_coord, String rest_tracking_num, int num_reports, List<Inspection> rest_inspectionList) {
-        this.rest_name = rest_name;
-        this.rest_addr = rest_addr;
-        this.GPS_coord = GPS_coord;
-        this.rest_tracking_num = rest_tracking_num;
-        this.num_reports = num_reports;
-        this.rest_inspectionList = rest_inspectionList;
+    public Restaurant(String trackingNumber, String name, String address, String city, double latitude, double longitude, List<Inspection> inspections) {
+        this.trackingNumber = trackingNumber;
+        this.name           = name;
+        this.address        = address;
+        this.city           = city;
+        this.latitude       = latitude;
+        this.longitude      = longitude;
+        // dependency injection
+        this.inspections    = inspections;
     }
 
-    //Getter and Setter
-    public String getRest_name() {
-        return rest_name;
+    public Restaurant(String trackingNumber, String name, String address, String city, double latitude, double longitude) {
+        this(trackingNumber, name, address, city, latitude, longitude, new ArrayList<Inspection>());
     }
 
-    public void setRest_name(String rest_name) {
-        this.rest_name = rest_name;
+
+    //////////////////////////////////////////////////////////////////////
+    // Delegate methods
+
+    public List<Inspection> getInspections() {
+        return Collections.unmodifiableList(inspections);
     }
 
-    public String getRest_addr() {
-        return rest_addr;
-    }
 
-    public void setRest_addr(String rest_addr) {
-        this.rest_addr = rest_addr;
-    }
-
-    public String getGPS_coord() {
-        return GPS_coord;
-    }
-
-    public void setGPS_coord(String GPS_coord) {
-        this.GPS_coord = GPS_coord;
-    }
-
-    public String getRest_tracking_num() {
-        return rest_tracking_num;
-    }
-
-    public void setRest_tracking_num(String rest_tracking_num) {
-        this.rest_tracking_num = rest_tracking_num;
-    }
-
-    public int getNum_reports() {
-        return num_reports;
-    }
-
-    public void setNum_reports(int num_reports) {
-        this.num_reports = num_reports;
-    }
-
-    public List<Inspection> getRest_inspectionList() {
-        return rest_inspectionList;
-    }
-
-    public void setRest_inspectionList(List<Inspection> rest_inspectionList) {
-        this.rest_inspectionList = rest_inspectionList;
+    // Used to populate the list of inspections; outside world shouldn't be able to add anything to list
+    protected void inspect(Inspection checkup) {
+        inspections.add(checkup);
     }
 }
--- app/src/main/java/ca/cmpt276/project/model/RestaurantManager.java
+++ app/src/main/java/ca/cmpt276/project/model/RestaurantManager.java
@@ -2,19 +2,22 @@ package ca.cmpt276.project.model;
 
 import androidx.annotation.NonNull;
 
-import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Iterator;
-import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
-public class RestaurantManager implements Iterable<Restaurant> {
+public class RestaurantManager {
     private static RestaurantManager instance = null;
-    private final List<Restaurant> restaurants;
+    private final Map<String, Restaurant> restaurants;      // maps Tracking Number -> Restaurant
 
     ////////////////////////////////////////////////////////////
     // Singleton pattern
 
     private RestaurantManager() {
-        restaurants = new ArrayList<>();
+        restaurants = new HashMap<>();
     }
 
     public RestaurantManager getInstance() {
@@ -28,29 +31,40 @@ public class RestaurantManager implements Iterable<Restaurant> {
     ////////////////////////////////////////////////////////////
     // Delegate methods
 
-    public Restaurant get(int index) {
-        return restaurants.get(index);
+    public boolean containsTrackingNumber(String trackingNumber) {
+        return restaurants.containsKey(trackingNumber);
     }
-    public int indexOf(Object restaurant) {
-        return restaurants.indexOf(restaurant);
+    public boolean containsRestaurant(Restaurant restaurant) {
+        return restaurants.containsValue(restaurant);
     }
-    public void add(Restaurant restaurant) {
-        restaurants.add(restaurant);
+
+    public Restaurant get(String trackingNumber) {
+        return restaurants.get(trackingNumber);
     }
-    public Restaurant remove(int index) {
-        return restaurants.remove(index);
+
+    public Set<String> trackingNumberSet() {
+        return Collections.unmodifiableSet(restaurants.keySet());
     }
-    public boolean remove(Object restaurant) {
-        return restaurants.remove(restaurant);
+    public Collection<Restaurant> restaurants() {
+        return Collections.unmodifiableCollection(restaurants.values());
     }
+    public Set<Map.Entry<String, Restaurant>> entrySet() {
+        return Collections.unmodifiableSet(restaurants.entrySet());
+    }
+
+    // Used when populating the RestuarantManager from CSV file
+    protected Restaurant put(String trackingNumber, Restaurant restaurant) {
+        return restaurants.put(trackingNumber, restaurant);
+    }
+
+    /* Shouldn't ever need to add/remove entries dynamically: all objects are encoded in dataset
+
+    public Restaurant remove(String trackingNumber) {
+        return restaurants.remove(trackingNumber);
+    }
+
     public void clear() {
         restaurants.clear();
     }
-
-
-    @NonNull
-    @Override
-    public Iterator<Restaurant> iterator() {
-        return restaurants.iterator();
-    }
+    */
 }
