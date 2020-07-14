69c7b3fb1b46b6d7456559c04a1656ec3179b6a6
dhpham <dayvidpham@gmail.com>
2020-07-13 19:40:09 -0700

    Adds class with static functions annotated with @TypeConverter to convert enums to Strings/ints

---
+++ app/src/main/java/ca/cmpt276/project/model/Converters.java
@@ -0,0 +1,41 @@
+package ca.cmpt276.project.model;
+
+import androidx.room.TypeConverter;
+
+public class Converters {
+    private Converters() throws InstantiationException {
+        throw new InstantiationException(getClass().getName() + " cannot be instantiated");
+    }
+
+    @TypeConverter
+    public static HazardRating hazardRatingFromString(String hazardRating) {
+        return HazardRating.fromString(hazardRating);
+    }
+    @TypeConverter
+    public static String hazardRatingToString(HazardRating hazardRating) {
+        return hazardRating.stringVal;
+    }
+
+    @TypeConverter
+    public static InspectionType inspectionTypeFromString(String inspection) {
+        return InspectionType.fromString(inspection);
+    }
+    @TypeConverter
+    public static String inspectionTypeToString(InspectionType type) {
+        return type.stringVal;
+    }
+
+    @TypeConverter
+    public static ViolationCategory violationCategoryFromCodeNumber(int codeNumber) {
+        return ViolationCategory.fromCodeNumber(codeNumber);
+    }
+    @TypeConverter
+    public static ViolationCategory violationCategoryFromString(String categoryStringVal) {
+        return ViolationCategory.fromString(categoryStringVal);
+    }
+    @TypeConverter
+    public static String violationCategoryToString(ViolationCategory category) {
+        return category.toString();
+    }
+
+}
