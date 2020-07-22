a0662090f2e4b91525521582c1421a4767842e91
nicolas <nandreas@sfu.ca>
2020-07-21 19:30:44 -0700

    Merge commit

--- app/src/main/java/ca/cmpt276/project/ui/MainActivity.java
+++ app/src/main/java/ca/cmpt276/project/ui/MainActivity.java
@@ -1,28 +1,47 @@
 package ca.cmpt276.project.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.lifecycle.ViewModelStore;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.LocalDateTime;
 
 import ca.cmpt276.project.R;
 
-import ca.cmpt276.project.model.HealthViewModel;
 import ca.cmpt276.project.model.RestaurantManager;
 
 
 public class MainActivity extends AppCompatActivity implements RestListAdapter.RestListClickListener {
     RestaurantManager manager;
     RecyclerView restList;
+
+    private static String REST_DL_URL;
+    private static final String REST_API_URL = "https://data.surrey.ca/api/3/action/package_show?id=restaurants";
+    private static LocalDateTime LAST_MODIFIED;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,8 +50,62 @@ public class MainActivity extends AppCompatActivity implements RestListAdapter.R
 
 
         updateUI();
-
+        //new JSONTask().execute(REST_API_URL);
     }
+
+
+        /*public class JSONTask extends AsyncTask< String, String, String>{
+
+            @Override
+            protected String doInBackground(String...params){
+                HttpURLConnection connection;
+                BufferedReader reader;
+                URL url = null;
+                try {
+                    url = new URL(params[0]);
+                    connection = (HttpURLConnection) url.openConnection();
+                    connection.connect();
+
+                    InputStream stream = connection.getInputStream();
+                    reader = new BufferedReader(new InputStreamReader(stream));
+
+                    StringBuffer stringBuffer = new StringBuffer();
+                    String line = "";
+                    while((line = reader.readLine()) != null){
+                        stringBuffer.append(line);
+                    }
+
+                    String finalJson = stringBuffer.toString();
+                    JSONObject parentObject = new JSONObject(finalJson);
+                    JSONObject childObject =  parentObject.getJSONObject("result");
+                    JSONArray parentArray = childObject.getJSONArray("resources");
+                    JSONObject targetObject = parentArray.getJSONObject(0);
+
+                    REST_DL_URL = targetObject.getString("url");
+                    LAST_MODIFIED = LocalDateTime.parse(targetObject.getString("last_modified"));
+
+//
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(String s){
+                super.onPostExecute(s);
+
+            }
+        }*/
+
+
+
+
+
 
     private void updateUI() {
         manager = RestaurantManager.getInstance(getApplicationContext());
